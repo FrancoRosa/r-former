5f949123d1cdba819b9d63da6ed55ba50cfd61c8
FrancoRosa <km115.franco@gmail.com>
2020-05-12 16:43:10 -0500

    Add edit controller/view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,14 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.new
+    @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path
+  end
   
 
   def new
@@ -21,7 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      redirect_to users_path
     else
       render :new
     end
