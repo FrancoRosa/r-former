510018bf273b1421f7f7786bb1d1a9870e9e5150
FrancoRosa <km115.franco@gmail.com>
2020-05-12 14:50:24 -0500

    Add placeholders and default values

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,34 @@
 class UsersController < ApplicationController
-  def new
+  def index
+    @user = User.all
   end
 
-  def create
+  def edit
+    @user = User.new
   end
+
+  
+
+  def new
+    @user = User.new
+  end
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
